ef9ae28bbfaf09c178bb76f8727d2bf3e6ea05f7
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2025-04-30 12:54:58 +0530

    Update Meow.sql

--- Meow.sql
+++ Meow.sql
@@ -23,10 +23,8 @@ END;
 /
 
 export ORACLE_SID=ORCL
+export ORACLE_BASE=/u01/app/oracle
 export ORACLE_HOME=/u01/app/oracle/product/12.2.0.1/dbhome_1
 export PATH=$ORACLE_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib
-export ORACLE_BASE=/u01/app/oracle
-
-
 
